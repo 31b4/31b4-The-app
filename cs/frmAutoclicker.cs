c6b0576bb69add82fff83dc638bf12a8b4f8f72a
31b4 <75566095+Bence314@users.noreply.github.com>
2021-09-04 02:14:18 +0200

    Update frmAutoclicker.cs

--- cs/frmAutoclicker.cs
+++ cs/frmAutoclicker.cs
@@ -14,10 +14,8 @@ namespace _31b4 {//project
     public partial class frmAutoclicker : Form {
         [DllImport("user32.dll")]
         static extern short GetAsyncKeyState(Keys vKey);
-
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
-
         private const int LEFTUP = 0x0004;
         private const int LEFTDOWN = 0x0002;
         public static int intervals = 50;
@@ -28,7 +26,6 @@ namespace _31b4 {//project
         }
         public static int testCounter = 0;
         public static Thread AC;
-
         private void frmAutoclicker_Load(object sender, EventArgs e) {//betoltes
             TestButtom.Text = Convert.ToString(testCounter);
             trackBar1.Maximum = 2000;
@@ -44,7 +41,6 @@ namespace _31b4 {//project
             Form1.ExitN = false;
             AC.Start();
         }
-
         private void AutoClick() {//auto clicker
             while (true) {
                 if (click == true) {
@@ -57,7 +53,6 @@ namespace _31b4 {//project
                 }
                 Thread.Sleep(2);
             }
-
         }
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
             while (true) {
@@ -65,18 +60,15 @@ namespace _31b4 {//project
                     if (GetAsyncKeyState(Keys.F9) < 0) {
                         click = false;
                         onoff.Text = "Off";
-
                     } else if (GetAsyncKeyState(Keys.F10) < 0) {
                         click = true;
                         onoff.Text = "On";
                     } else if(!click){
                         onoff.Text = "Off";
-
                     } else if (click) {
                         onoff.Text = "On";
                     }
                     Thread.Sleep(1);
-                    
                 } else if (!checkBox1.Checked) {
                     click = false;
                     onoff.Text = "Off";
@@ -89,7 +81,6 @@ namespace _31b4 {//project
                 } else if (pressToRun.Checked) {
                     click = false;
                 }
-                
             }
         }//end background worker
         private void trackBar1_Scroll(object sender, EventArgs e) {
@@ -97,29 +88,24 @@ namespace _31b4 {//project
             timer1.Interval = trackBar1.Value;
             intervals = trackBar1.Value;
         }
-
         private void TestButtom_Click(object sender, EventArgs e) {
             testCounter++;
             TestButtom.Text = Convert.ToString(testCounter);
         }
-
         private void button1_Click(object sender, EventArgs e) {
             testCounter = 0;
             TestButtom.Text = Convert.ToString(testCounter);
         }
-
         private void onoff_Click(object sender, EventArgs e) {
             if (checkBox1.Checked && !(pressToRun.Checked)) {
                 if (onoff.Text=="Off") {
                     onoff.Text = "On";
                     Thread.Sleep(1000);
                     click = true;
-
                 } else {
                     onoff.Text = "Off";
                     click = false;
                     Thread.Sleep(500);
-
                 }
             }
         }
